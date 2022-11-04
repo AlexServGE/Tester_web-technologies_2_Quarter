0fd87c6785e14f266ffc9a453a3ea6541935bdec
AlexServGE <alexander.sergeevru@yandex.ru>
2022-11-04 15:41:57 +0300

    parseInt added

--- Homework3/Task1/myscript.js
+++ Homework3/Task1/myscript.js
@@ -1,3 +1,3 @@
-var celciusTemperature = prompt("Введите значение температуры в градусах Цельсия: ");
+var celciusTemperature = parseInt(prompt("Введите значение температуры в градусах Цельсия: "));
 var result = ((9 / 5) * celciusTemperature + 32).toFixed(1);
 alert(`Цельсий: ${celciusTemperature}, Фаренгейт: ${result}`)
